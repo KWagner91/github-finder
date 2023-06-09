1c14ad9194efc82beac68fb1a066462fb69e1fc3
Kerstin Bonev-Wagner <k.bonev-wagner@gmx.at>
2023-06-09 10:36:17 +0200

    Displaying Github Details of searched user

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -8,6 +8,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     isLoading: false,
   };
 
@@ -43,6 +44,24 @@ export const GithubProvider = ({ children }) => {
     }
   };
 
+  const getUserRepos = async (login) => {
+    setLoading();
+    const params = new URLSearchParams({
+      sort: 'created',
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`
+    );
+
+    const data = await response.json();
+    dispatch({
+      type: 'GET_REPOS',
+      payload: data,
+    });
+  };
+
   const clearUsers = () => {
     dispatch({
       type: 'CLEAR_USERS',
@@ -57,9 +76,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         user: state.user,
         isLoading: state.loading,
+        repos: state.repos,
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
       }}
     >
       {children}
